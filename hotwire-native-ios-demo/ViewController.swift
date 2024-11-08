1b5ade5cf2770a5f05da0fe9c1057c0e70bc6bbb
Harshal Bhakta <harshal@cital.co>
2024-11-08 17:20:28 +0530

    Initial Commit

---
+++ hotwire-native-ios-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotwire-native-ios-demo
+//
+//  Created by Harshal Bhakta on 08/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hotwire-native-ios-demoTests/hotwire_native_ios_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  hotwire_native_ios_demoTests.swift
+//  hotwire-native-ios-demoTests
+//
+//  Created by Harshal Bhakta on 08/11/24.
+//
+
+import Testing
+@testable import hotwire_native_ios_demo
+
+struct hotwire_native_ios_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
